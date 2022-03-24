b625cf6243eee887d209e006c603a2718f5b1cd7
mellito <dayanalexismanrique@hotmail.com>
2022-03-24 00:31:10 -0500

    change image direcccion

--- src/pages/Blogs/index.jsx
+++ src/pages/Blogs/index.jsx
@@ -3,16 +3,15 @@ import Pacman from "../../components/Pacman";
 import { Link } from "react-router-dom";
 import BlogsData from "../../assets/blogs.json";
 import "./blogs.scss";
-import Rarenew from "../../assets/img/1078280-tech.webp";
 
 function Blogs() {
   const lastPost = BlogsData.find((blog) => !!blog.lastpost);
 
   return (
-    <main class="blogs-main">
+    <main className="blogs-main">
       <section className="blogs-news-container">
         <figure className="blogs-news-img-container pixel-box--primary ">
-          <img src={Rarenew} alt={lastPost.title} />
+          <img src={lastPost.image} alt={lastPost.title} />
         </figure>
         <div className="blogs-news-info-container">
           <h2 className="">{lastPost.title}</h2>
